88abeb184b002271037155899961cbc1480789d6
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-12-22 21:12:29 +0100

    finish

--- src/components/Loading/Loading.jsx
+++
@@ -1,8 +0,0 @@
-// import s from './Loader.module.css';
-// import { Component } from 'react';
-// class loader extends Component {
-//   render() {
-//     return <div className={s.loader}></div>;
-//   }
-// }
-// export default loader;
