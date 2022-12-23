d4edaae6946f19ef5008436a325ff0ff92eb9929
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-12-23 16:43:24 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,23 @@ export class App extends Component {
     images: [],
     page: 0,
   };
+  handleSubmit = searchData => {
+    if (searchData.trim() === '') {
+      return console.log('Enter the meaning for search');
+    } else if (searchData === this.state.searchData) {
+      return;
+    }
+    this.setState({
+      searchData: searchData,
+      page: 1,
+      images: [],
+    });
+  };
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSubmit={this.handleSubmit} />
       </div>
     );
   }
