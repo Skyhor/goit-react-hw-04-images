a9ed130a21f4b9b37efec33c8e20c9d84f54adef
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-12-23 16:04:22 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SearchBar from './Searchbar/Searchbar';
+// import SearchBar from './Searchbar/Searchbar';
 export const App = () => {
   state = {
     searchData: '',
