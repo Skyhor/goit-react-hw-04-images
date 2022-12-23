6d1179c37df06177f05ed7d139a622fe03990736
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-12-23 16:02:59 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,26 @@
+import React, { Component } from 'react';
+import SearchBar from './Searchbar/Searchbar';
 export const App = () => {
-  return <div>React homework template</div>;
+  state = {
+    searchData: '',
+    page: 0,
+    images: [],
+  };
+  handleSubmit = searchData => {
+    if (searchData.trim() === '') {
+      return toast.error('Enter the meaning for search');
+    } else if (searchData === this.state.searchData) {
+      return;
+    }
+    this.setState({
+      searchData: searchData,
+      page: 1,
+      images: [],
+    });
+  };
+  return (
+    <div>
+      <SearchBar onSubmit={this.handleSubmit} />
+    </div>
+  );
 };
