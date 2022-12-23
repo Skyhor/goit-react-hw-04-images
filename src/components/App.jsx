501f3b8dcc1c0dee5fc97178ba040582df9912d0
Yehor Skibnevskyi <105387497+Skyhor@users.noreply.github.com>
2022-12-23 16:26:22 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Component } from 'react';
-import SearchBar from './Searchbar/Searchbar';
-import { ToastContainer, toast } from 'react-toastify';
+
 export class App extends Component {
   state = {
     searchData: '',
